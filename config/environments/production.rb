d98dab00dacbd0fbb27a79f029b012a652c4ea56
kai langrehr <kai.langrehr@gmail.com>
2019-10-22 01:58:06 +0000

    Lesson 44 mailing on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,6 +11,8 @@ Rails.application.configure do
     enable_starttls_auto: true
   }
 
+  config.action_mailer.default_url_options = { host: 'nomster-kai-langrehr.herokuapp.com' }
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
